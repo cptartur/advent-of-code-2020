ca353a3f24f6352a324cff6ab8434e7d19a08f19
cptartur <artumi1@gmail.com>
2020-12-15 23:50:06 +0100

    add solutions for day 15

---
+++ days/15/15.2.py
@@ -0,0 +1,13 @@
+t = (2,15,0,9,1,20)
+d = {v: [i, -1] for i, v in enumerate(t, start=1)}
+last = t[-1]
+for i in range(len(t) + 1, 30000001):
+    if (last in d and d[last][1] == -1) or last not in d:
+        d[0][1], d[0][0] = d[0][0], i
+        last = 0
+    else:
+        last = d[last][0] - d[last][1]
+        if last not in d:
+            d[last] = [-1, -1]
+        d[last][1], d[last][0] = d[last][0], i
+print(last)
